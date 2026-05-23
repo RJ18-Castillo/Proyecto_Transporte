c49d09e1473ae0b9249ebdae20b8a77fb8560d07
Martin Angulo <chavezangulo9@gmail.com>
2026-05-23 13:34:06 -0600

    Se me habia olvidado subir la logica de los modelos en el IGestor Transporte.

--- Transporte.BL/IGestorTransporte.cs
+++ Transporte.BL/IGestorTransporte.cs
@@ -4,25 +4,32 @@ namespace Transporte.BL
 {
     public interface IGestorTransporte
     {
-        Usuario Login(
-            string usuario,
-            string clave);
+        Usuario Login(string usuario, string clave);
 
-        bool CambiarClave(
-            string usuario,
-            string claveActual,
-            string claveNueva);
+        bool CambiarClave(string usuario, string claveActual, string claveNueva);
 
-        List<Chofer> ListarChoferes(
-            string filtro);
+        List<Chofer> ListarChoferes(string filtro);
 
-        Chofer ObtenerChofer(
-            int id);
+        Chofer ObtenerChofer(int id);
 
-        void AgregarChofer(
-            Chofer chofer);
+        void AgregarChofer(Chofer chofer);
 
-        void EditarChofer(
-            Chofer chofer);
+        void EditarChofer(Chofer chofer);
+
+        List<Pasajero> ListarPasajeros(string filtro);
+        Pasajero ObtenerPasajero(int id);
+        void AgregarPasajero(Pasajero pasajero);
+        void EditarPasajero(Pasajero pasajero);
+
+        List<Ruta> ListarRutas(string filtro);
+        Ruta ObtenerRuta(int id);
+        void AgregarRuta(Ruta ruta);
+        void EditarRuta(Ruta ruta);
+
+        List<Unidad> ListarUnidades();
+        Unidad ObtenerUnidad(int id);
+        bool ExistePlaca(string placa, int idIgnorar = 0);
+        bool AgregarUnidad(Unidad unidad);
+        bool EditarUnidad(Unidad unidad);
     }
 }
