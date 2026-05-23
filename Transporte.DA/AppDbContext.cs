78171230aaaf300c6dfb33d4a8f9597438737225
Martin Angulo <chavezangulo9@gmail.com>
2026-05-23 13:14:12 -0600

    Se modifico el archivos AppDbContext para agregar los nuevos modelos.

--- Transporte.DA/AppDbContext.cs
+++ Transporte.DA/AppDbContext.cs
@@ -5,26 +5,32 @@ namespace Transporte.DA
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(
-            DbContextOptions<AppDbContext> options)
-            : base(options)
-        {
-        }
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options){ }
 
         public DbSet<Usuario> Usuarios { get; set; }
-
         public DbSet<Chofer> Choferes { get; set; }
+        public DbSet<Pasajero> Pasajeros { get; set; }
+        public DbSet<Ruta> Rutas { get; set; }
+        public DbSet<Unidad> Unidades { get; set; }
 
-        protected override void OnModelCreating(
-            ModelBuilder modelBuilder)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Usuario>()
-                .HasIndex(u => u.NombreUsuario)
+                .HasIndex(usuario => usuario.NombreUsuario)
                 .IsUnique();
 
             modelBuilder.Entity<Usuario>()
-                .HasIndex(u => u.Correo)
+                .HasIndex(usuario => usuario.Correo)
                 .IsUnique();
+
+            modelBuilder.Entity<Unidad>()
+                .HasIndex(unidad => unidad.Placa)
+                .IsUnique();
+
+            modelBuilder.Entity<Pasajero>()
+                .HasOne(pasajero => pasajero.Usuario)
+                .WithMany()
+                .HasForeignKey(pasajero => pasajero.UsuarioId);
 
             base.OnModelCreating(modelBuilder);
         }
